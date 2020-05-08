bf350278f97da44e89e3b21a7738c657829512ef
philipstears <philip@philipstears.com>
2020-05-08 18:26:48 +0100

    Workspace.

---
+++ shell.nix
@@ -0,0 +1,38 @@
+let
+  mozilla =
+    builtins.fetchGit {
+      name = "nixpkgs-mozilla";
+      url = https://github.com/mozilla/nixpkgs-mozilla/;
+    };
+
+  nixPackages =
+    import <nixpkgs> {
+      overlays = [
+        (import mozilla)
+      ];
+    };
+
+  rust =
+    (nixPackages.latest.rustChannels.stable.rust.override {
+      extensions = [
+        "rust-src"
+        "rls-preview"
+        "rust-analysis"
+        "rustfmt-preview"
+        "clippy-preview"
+      ];
+    });
+
+in
+
+  with nixPackages;
+
+  mkShell {
+    buildInputs = with pkgs; [
+      rust
+      rustracer
+    ];
+
+    RUST_SRC_PATH = "${rust}/lib/rustlib/src/rust/src";
+    RACER_PATH = "${rustracer}/bin/racer";
+  }
