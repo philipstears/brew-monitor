b7e6c57a4e2b49ec37f35b9fdb92c0d6ff80bcaa
philipstears <philip@philipstears.com>
2020-12-03 20:09:10 +0000

    Controls to turn on and off heat/pump

--- bm/www_src/gf/index.tsx
+++ bm/www_src/gf/index.tsx
@@ -5,6 +5,10 @@ export interface GrainfatherProps {
 }
 
 export interface GrainfatherState {
+    command_url: string;
+    recipe_url: string;
+    ws_url: string;
+
     status1: Proto.Status1Data;
     status2: Proto.Status2Data;
     temp: Proto.TempData;
@@ -15,20 +19,24 @@ export class Grainfather extends React.Component<GrainfatherProps, GrainfatherSt
         super(props);
 
         this.state = {
+            command_url: `${window.location.protocol}//${window.location.host}/gf/command`,
+            recipe_url: `${window.location.protocol}//${window.location.host}/gf/recipe`,
+            ws_url: `ws://${window.location.host}/gf/ws`,
+
             status1: { heat_active: false, pump_active: false },
             status2: {},
             temp: { desired: 0, current: 0 },
         };
 
-        let ws = new WebSocket(`ws://${window.location.host}/gf/ws`);
+        let ws = new WebSocket(this.state.ws_url);
         ws.onmessage = event => this.handleWebSocketMessage(event);
     }
 
     render = () => (
         <React.Fragment>
             <div>Grainfather!</div>
-            <div>{this.state.status1.heat_active ? "Heat On" : "Heat Off"}</div>
-            <div>{this.state.status1.pump_active ? "Pump On" : "Pump Off"}</div>
+            <div><Heat command_url={this.state.command_url} data={this.state.status1} /></div>
+            <div><Pump command_url={this.state.command_url} data={this.state.status1} /></div>
             <div>{this.state.temp.current} / {this.state.temp.desired}</div>
         </React.Fragment>
     );
@@ -50,3 +58,51 @@ export class Grainfather extends React.Component<GrainfatherProps, GrainfatherSt
     };
 }
 
+export interface GrainfatherData<T> {
+    command_url: string,
+    data: T;
+}
+
+export class Pump extends React.Component<GrainfatherData<Proto.Status1Data>, {}> {
+    render = () => (
+        <button onClick={this.handleClick}>
+            {this.props.data.pump_active ? "Pump On" : "Pump Off"}
+        </button>
+    );
+
+    handleClick = async () => {
+        let command = {
+            type: "TogglePumpActive",
+        };
+
+        await fetch(this.props.command_url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(command),
+        });
+    };
+}
+
+export class Heat extends React.Component<GrainfatherData<Proto.Status1Data>, {}> {
+    render = () => (
+        <button onClick={this.handleClick}>
+            {this.props.data.heat_active ? "Heat On" : "Heat Off"}
+        </button>
+    );
+
+    handleClick = async () => {
+        let command = {
+            type: "ToggleHeatActive",
+        };
+
+        await fetch(this.props.command_url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(command),
+        });
+    };
+}
