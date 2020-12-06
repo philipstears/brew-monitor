e4c9321341a9a55e40c2d4ce7b44cf273c47bdbb
philipstears <philip@philipstears.com>
2020-12-06 13:25:49 +0000

    More documentation

--- bm-grainfather/src/bluetooth.rs
+++ bm-grainfather/src/bluetooth.rs
@@ -3,10 +3,19 @@ pub mod btleplug;
 
 use bm_bluetooth::*;
 
+/// The service identifier for the grainfather controller.
 pub const SERVICE_ID: u128 = 0x0000cdd000001000800000805f9b34fb;
+
+/// The identifier of the characteristic used for receiving notifications
+/// from the grainfather controller.
 pub const CHARACTERISTIC_ID_READ: u128 = 0x0003cdd100001000800000805f9b0131;
+
+/// The identifier of the characteristic used to issue commands to the
+/// grainfather controller.
 pub const CHARACTERISTIC_ID_WRITE: u128 = 0x0003cdd200001000800000805f9b0131;
 
+/// Searches for the presence of the Grainfather's [service id](crate::SERVICE_ID)
+/// in the provided extended information report.
 pub fn has_grainfather_service_id(report: &EIRData) -> bool {
     report.into_iter().any(|entry| {
         if let EIREntry::ServiceIds(ids) = entry {
--- bm-grainfather/src/lib.rs
+++ bm-grainfather/src/lib.rs
@@ -1,3 +1,24 @@
+//! This crate provides facilities for communicating with a Grainfather controller.
+//!
+//! There are broadly 3 components to this library, protocol parsing/unparsing,
+//! bluetooth helpers, and an implementation of a client using the btleplug
+//! bluetooth library.
+//!
+//! [Command](crate::Command), [Notification](crate::Notification), and
+//! [Recipe](crate::Recipe) are the three principal protocol-level types
+//! and support the parsing/unparsing of those entities.
+//!
+//! The bluetooth helpers include the [service id](crate::SERVICE_ID), and
+//! [read](crate::CHARACTERISTIC_ID_READ)/[write](crate::CHARACTERISTIC_ID_WRITE)
+//! characteristic ids, as well as a [helper](crate::has_grainfather_service_id) that
+//! can be used to see if the service id is contained within an Extended Information
+//! Report returned by a bluetooth library.
+//!
+//! Finally, there's a `btleplug` feature (turned on by default), which makes
+//! a [client](crate::btleplug::Client) available which wraps a `btleplug::api::Peripheral`
+//! and exposes an API for interacting with the Grainfather Controller in terms
+//! of commands, notifications, and recipes.
+
 // TODO: review temperature units
 pub mod calc;
 
