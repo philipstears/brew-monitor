2e8d4188d0070b71e253bb433e66c27f793b6848
philipstears <philip@philipstears.com>
2020-12-11 20:54:27 +0000

    Move state tracking from bm-grainfather to bm

--- bm-grainfather/src/bluetooth/btleplug.rs
+++ bm-grainfather/src/bluetooth/btleplug.rs
@@ -1,17 +1,14 @@
 //! Provides a client to make it easy to work with a Grainfather controller.
 
 use crate::bluetooth::*;
-use crate::{notifications::*, Command, InteractionCode, Notification, Recipe, StepNumber};
+use crate::{Command, Notification, Recipe};
 
 use ::btleplug::{
     api::{Characteristic, NotificationHandler as BtlePlugNotificationHandler, Peripheral, UUID},
     Error, Result as BtlePlugResult,
 };
 
-use std::{
-    convert::TryFrom,
-    sync::{Arc, RwLock},
-};
+use std::convert::TryFrom;
 
 pub type NotificationHandler = Box<dyn FnMut(Notification) + Send>;
 
@@ -85,37 +82,11 @@ where
     }
 }
 
-#[derive(Debug, Default)]
-struct State {
-    // Status 1
-    heat_active: bool,
-    pump_active: bool,
-    auto_mode_active: bool,
-    step_ramp_active: bool,
-    interaction_mode_active: bool,
-    interaction_code: InteractionCode,
-    step_number: StepNumber,
-    delayed_heat_mode_active: bool,
-    // Status 2
-    heat_power_output_percentage: u8,
-    timer_paused: bool,
-    step_mash_mode: bool,
-    recipe_interrupted: bool,
-    manual_power_mode: bool,
-    sparge_water_alert_displayed: bool,
-    // Temp
-    temp_desired: i32,
-    temp_current: i32,
-    // Timer
-    timer_active: bool,
-}
-
 #[derive(Debug)]
 pub struct Client {
     gf: Box<dyn ClientImpl>,
     read: Characteristic,
     write: Characteristic,
-    state: Arc<RwLock<State>>,
 }
 
 impl Client {
@@ -142,99 +113,11 @@ impl Client {
         let wc_id = UUID::B128(CHARACTERISTIC_ID_WRITE.to_le_bytes());
         let wc = cs.iter().find(|c| c.uuid == wc_id).ok_or(ClientError::WriteCharacteristic)?;
 
-        let state = Arc::new(RwLock::new(State::default()));
-
         let result = Self {
             gf,
             read: rc.clone(),
             write: wc.clone(),
-            state: state.clone(),
         };
-
-        result
-            .subscribe(Box::new(move |notification| match notification {
-                Notification::Status1(Status1 {
-                    heat_active,
-                    pump_active,
-                    auto_mode_active,
-                    step_ramp_active,
-                    interaction_mode_active,
-                    interaction_code,
-                    step_number,
-                    delayed_heat_mode_active,
-                }) => {
-                    let mut state = state.write().unwrap();
-
-                    maybe_update("heat_active", &mut state.heat_active, &heat_active);
-                    maybe_update("pump_active", &mut state.pump_active, &pump_active);
-                    maybe_update("auto_mode_active", &mut state.auto_mode_active, &auto_mode_active);
-                    maybe_update("step_ramp_active", &mut state.step_ramp_active, &step_ramp_active);
-                    maybe_update(
-                        "interaction_mode_active",
-                        &mut state.interaction_mode_active,
-                        &interaction_mode_active,
-                    );
-                    maybe_update("interaction_code", &mut state.interaction_code, &interaction_code);
-                    maybe_update("step_number", &mut state.step_number, &step_number);
-                    maybe_update(
-                        "delayed_heat_mode_active",
-                        &mut state.delayed_heat_mode_active,
-                        &delayed_heat_mode_active,
-                    );
-                }
-                Notification::Status2(Status2 {
-                    heat_power_output_percentage,
-                    timer_paused,
-                    step_mash_mode,
-                    recipe_interrupted,
-                    manual_power_mode,
-                    sparge_water_alert_displayed,
-                }) => {
-                    let mut state = state.write().unwrap();
-
-                    maybe_update(
-                        "heat_power_output_percentage",
-                        &mut state.heat_power_output_percentage,
-                        &heat_power_output_percentage,
-                    );
-                    maybe_update("timer_paused", &mut state.timer_paused, &timer_paused);
-                    maybe_update("step_mash_mode", &mut state.step_mash_mode, &step_mash_mode);
-                    maybe_update("recipe_interrupted", &mut state.recipe_interrupted, &recipe_interrupted);
-                    maybe_update("manual_power_mode", &mut state.manual_power_mode, &manual_power_mode);
-                    maybe_update(
-                        "sparge_water_alert_displayed",
-                        &mut state.sparge_water_alert_displayed,
-                        &sparge_water_alert_displayed,
-                    );
-                }
-                Notification::Temp(Temp {
-                    desired,
-                    current,
-                }) => {
-                    let mut state = state.write().unwrap();
-
-                    // These frequently fluctuate by 0.1 of a degree, which is
-                    // annoying to report, so don't
-                    state.temp_desired = (desired * 100.0) as i32;
-                    state.temp_current = (current * 100.0) as i32;
-                }
-                Notification::DelayedHeatTimer(Timer {
-                    active,
-                    ..
-                }) => {
-                    let mut state = state.write().unwrap();
-                    maybe_update("timer_active", &mut state.timer_active, &active);
-                }
-                Notification::Interaction(Interaction {
-                    interaction_code,
-                }) => {
-                    println!("[R]: interaction with code {:?}", interaction_code);
-                }
-                other => {
-                    println!("[R]: {:?}", other);
-                }
-            }))
-            .unwrap();
 
         Ok(result)
     }
@@ -282,17 +165,4 @@ impl Client {
 
         self.gf.subscribe(&self.read)
     }
-}
-
-fn maybe_update<T>(field_name: &str, target: &mut T, new_value: &T)
-where
-    T: Eq + Clone + std::fmt::Debug,
-{
-    if target == new_value {
-        return;
-    }
-
-    println!("[R]: {} changed from {:?} to {:?}", field_name, target, new_value);
-
-    *target = new_value.clone();
 }
--- bm/src/devices/gf_manager.rs
+++ bm/src/devices/gf_manager.rs
@@ -1,4 +1,6 @@
-use bm_grainfather::{btleplug::Client as GrainfatherClient, Command, Notification, Recipe};
+use bm_grainfather::{
+    btleplug::Client as GrainfatherClient, notifications::*, Command, InteractionCode, Notification, Recipe, StepNumber,
+};
 use std::sync::{
     mpsc::{self, Receiver, Sender},
     Arc, Mutex, MutexGuard,
@@ -36,6 +38,7 @@ impl GrainfatherManager {
 struct GrainfatherInternal {
     client: Option<GrainfatherClient>,
     subscribers: Arc<Mutex<Vec<Sender<Notification>>>>,
+    state: Arc<Mutex<State>>,
 }
 
 impl GrainfatherInternal {
@@ -45,6 +48,7 @@ impl GrainfatherInternal {
         Self {
             client: None,
             subscribers: Arc::new(Mutex::new(Vec::with_capacity(Self::INITIAL_HANDLER_CAPACITY))),
+            state: Arc::new(Mutex::new(State::default())),
         }
     }
 
@@ -55,6 +59,7 @@ impl GrainfatherInternal {
             println!("Setting grainfather");
 
             let subscribers = self.subscribers.clone();
+            let state = self.state.clone();
 
             client
                 .subscribe(Box::new(move |notification| {
@@ -62,6 +67,8 @@ impl GrainfatherInternal {
                         let keep_subscriber = subscriber.send(notification.clone()).is_ok();
                         keep_subscriber
                     });
+
+                    state.lock().unwrap().update(notification);
                 }))
                 .unwrap();
 
@@ -84,4 +91,114 @@ impl GrainfatherInternal {
         self.subscribers.lock().unwrap().push(sender);
         receiver
     }
+}
+
+#[derive(Debug, Default)]
+struct State {
+    // Status 1
+    heat_active: bool,
+    pump_active: bool,
+    auto_mode_active: bool,
+    step_ramp_active: bool,
+    interaction_mode_active: bool,
+    interaction_code: InteractionCode,
+    step_number: StepNumber,
+    delayed_heat_mode_active: bool,
+    // Status 2
+    heat_power_output_percentage: u8,
+    timer_paused: bool,
+    step_mash_mode: bool,
+    recipe_interrupted: bool,
+    manual_power_mode: bool,
+    sparge_water_alert_displayed: bool,
+    // Temp
+    temp_desired: i32,
+    temp_current: i32,
+    // Timer
+    timer_active: bool,
+}
+
+impl State {
+    fn update(&mut self, notification: Notification) {
+        match notification {
+            Notification::Status1(Status1 {
+                heat_active,
+                pump_active,
+                auto_mode_active,
+                step_ramp_active,
+                interaction_mode_active,
+                interaction_code,
+                step_number,
+                delayed_heat_mode_active,
+            }) => {
+                maybe_update("heat_active", &mut self.heat_active, &heat_active);
+                maybe_update("pump_active", &mut self.pump_active, &pump_active);
+                maybe_update("auto_mode_active", &mut self.auto_mode_active, &auto_mode_active);
+                maybe_update("step_ramp_active", &mut self.step_ramp_active, &step_ramp_active);
+                maybe_update("interaction_mode_active", &mut self.interaction_mode_active, &interaction_mode_active);
+                maybe_update("interaction_code", &mut self.interaction_code, &interaction_code);
+                maybe_update("step_number", &mut self.step_number, &step_number);
+                maybe_update("delayed_heat_mode_active", &mut self.delayed_heat_mode_active, &delayed_heat_mode_active);
+            }
+            Notification::Status2(Status2 {
+                heat_power_output_percentage,
+                timer_paused,
+                step_mash_mode,
+                recipe_interrupted,
+                manual_power_mode,
+                sparge_water_alert_displayed,
+            }) => {
+                maybe_update(
+                    "heat_power_output_percentage",
+                    &mut self.heat_power_output_percentage,
+                    &heat_power_output_percentage,
+                );
+                maybe_update("timer_paused", &mut self.timer_paused, &timer_paused);
+                maybe_update("step_mash_mode", &mut self.step_mash_mode, &step_mash_mode);
+                maybe_update("recipe_interrupted", &mut self.recipe_interrupted, &recipe_interrupted);
+                maybe_update("manual_power_mode", &mut self.manual_power_mode, &manual_power_mode);
+                maybe_update(
+                    "sparge_water_alert_displayed",
+                    &mut self.sparge_water_alert_displayed,
+                    &sparge_water_alert_displayed,
+                );
+            }
+            Notification::Temp(Temp {
+                desired,
+                current,
+            }) => {
+                // These frequently fluctuate by 0.1 of a degree, which is
+                // annoying to report, so don't
+                self.temp_desired = (desired * 100.0) as i32;
+                self.temp_current = (current * 100.0) as i32;
+            }
+            Notification::DelayedHeatTimer(Timer {
+                active,
+                ..
+            }) => {
+                maybe_update("timer_active", &mut self.timer_active, &active);
+            }
+            Notification::Interaction(Interaction {
+                interaction_code,
+            }) => {
+                println!("[R]: interaction with code {:?}", interaction_code);
+            }
+            other => {
+                println!("[R]: {:?}", other);
+            }
+        }
+    }
+}
+
+fn maybe_update<T>(field_name: &str, target: &mut T, new_value: &T)
+where
+    T: Eq + Clone + std::fmt::Debug,
+{
+    if target == new_value {
+        return;
+    }
+
+    println!("[R]: {} changed from {:?} to {:?}", field_name, target, new_value);
+
+    *target = new_value.clone();
 }
