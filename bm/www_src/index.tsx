1a33609ccf8e7aeae4e62ece4c2c1dc43c2a2b76
philipstears <philip@philipstears.com>
2020-12-17 12:40:46 +0000

    Move the app into its own directory

--- bm/www_src/index.tsx
+++ bm/www_src/index.tsx
@@ -3,7 +3,7 @@ import "./index.css";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import * as Modal from "react-modal";
-import { App } from "./app";
+import { App } from "./app/index";
 
 Modal.setAppElement("#bm-app");
 
