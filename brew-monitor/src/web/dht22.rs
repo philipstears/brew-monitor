f33b274de9385f3a6389b81bea4897978d57f34e
philipstears <philip@philipstears.com>
2021-01-11 08:59:17 +0000

    404 if the DHT22 isn't there

--- brew-monitor/src/web/dht22.rs
+++ brew-monitor/src/web/dht22.rs
@@ -1,5 +1,6 @@
 use bm_db::DB;
 use chrono::{DateTime, Utc};
+use futures::future;
 use serde::{Deserialize, Serialize};
 use warp::{reject::Rejection, reply::Reply, Filter};
 
@@ -10,12 +11,16 @@ struct ReadingsQuery {
 }
 
 pub fn route(db: DB) -> impl Filter<Extract = impl Reply, Error = Rejection> + Clone {
-    let readings = warp::path!("dht22" / String).and(warp::query::<ReadingsQuery>()).map(
+    let readings = warp::path!("dht22" / String).and(warp::query::<ReadingsQuery>()).and_then(
         move |alias: String, query: ReadingsQuery| {
             let maybe_dht22 = db.dht22_try_get(alias.as_str()).unwrap();
-            let dht22 = maybe_dht22.unwrap();
-            let readings = dht22.get_readings(query.from, query.to).unwrap();
-            Ok(warp::reply::json(&readings))
+
+            maybe_dht22
+                .map(|dht22| {
+                    let readings = dht22.get_readings(query.from, query.to).unwrap();
+                    future::ok(warp::reply::json(&readings))
+                })
+                .unwrap_or_else(|| future::err(warp::reject::not_found()))
         },
     );
 
