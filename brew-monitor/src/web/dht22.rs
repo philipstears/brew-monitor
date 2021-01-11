a710e95cf0dee46a05747757e2bc16b52648ec9c
philipstears <philip@philipstears.com>
2021-01-11 23:44:43 +0000

    Fix getting a 405 instead of a 404 when GETting a non-existent DHT22

--- brew-monitor/src/web/dht22.rs
+++ brew-monitor/src/web/dht22.rs
@@ -1,6 +1,5 @@
 use bm_db::{DHT22Info, DB};
 use chrono::{DateTime, Utc};
-use futures::future;
 use serde::{Deserialize, Serialize};
 use warp::{reject::Rejection, reply::Reply, Filter};
 
@@ -11,24 +10,22 @@ struct ReadingsQuery {
 }
 
 pub fn route(db: DB) -> impl Filter<Extract = impl Reply, Error = Rejection> + Clone {
-    let sensor = warp::path::param::<String>();
+    warp::path::path("dht22").and(routes::put(db.clone()).or(routes::get(db.clone())).or(routes::readings(db.clone())))
+}
 
-    let get = {
-        let db = db.clone();
+fn with_db(db: DB) -> impl Filter<Extract = (DB,), Error = std::convert::Infallible> + Clone {
+    warp::any().map(move || db.clone())
+}
 
-        sensor.and(warp::path::end()).and(warp::filters::method::get()).and_then(move |alias: String| {
-            let maybe_dht22 = db.dht22().try_get_info(alias.as_str()).unwrap();
+mod routes {
+    use super::*;
 
-            maybe_dht22
-                .map(|dht22| future::ok(warp::reply::json(&dht22)))
-                .unwrap_or_else(|| future::err(warp::reject::not_found()))
-        })
-    };
+    pub(super) fn get(db: DB) -> impl Filter<Extract = impl Reply, Error = Rejection> + Clone {
+        warp::path!(String).and(warp::filters::method::get()).and(with_db(db)).and_then(handlers::get)
+    }
 
-    let put = {
-        let db = db.clone();
-        sensor
-            .and(warp::path::end())
+    pub(super) fn put(db: DB) -> impl Filter<Extract = impl Reply, Error = Rejection> + Clone {
+        warp::path!(String)
             .and(warp::filters::method::put())
             .and(warp::body::content_length_limit(1024))
             .and(warp::body::json())
@@ -37,15 +34,28 @@ pub fn route(db: DB) -> impl Filter<Extract = impl Reply, Error = Rejection> + C
                 // TODO: 204 if it already existed?
                 warp::reply::with_status(warp::reply::reply(), warp::http::StatusCode::CREATED)
             })
-    };
+    }
 
-    let readings = sensor.and(warp::path!("readings")).and(warp::query::<ReadingsQuery>()).map(
-        move |alias: String, query: ReadingsQuery| {
-            // TODO: if there are no readings, we should check if the device is registered
-            let readings = db.dht22().get_readings(alias.as_str(), query.from, query.to).unwrap();
-            Ok(warp::reply::json(&readings))
-        },
-    );
+    pub(super) fn readings(db: DB) -> impl Filter<Extract = impl Reply, Error = Rejection> + Clone {
+        warp::path!(String / "readings").and(warp::query::<ReadingsQuery>()).map(
+            move |alias: String, query: ReadingsQuery| {
+                // TODO: if there are no readings, we should check if the device is registered
+                let readings = db.dht22().get_readings(alias.as_str(), query.from, query.to).unwrap();
+                Ok(warp::reply::json(&readings))
+            },
+        )
+    }
+}
 
-    warp::path::path("dht22").and(get.or(put).or(readings))
+mod handlers {
+    use super::*;
+
+    pub(super) async fn get(alias: String, db: DB) -> Result<Box<dyn Reply>, Rejection> {
+        let reply: Box<dyn Reply> = match db.dht22().try_get_info(alias.as_str()).unwrap() {
+            Some(dht22) => Box::new(warp::reply::json(&dht22)),
+            None => Box::new(warp::reply::with_status(warp::reply(), warp::http::StatusCode::NOT_FOUND)),
+        };
+
+        Ok(reply)
+    }
 }
